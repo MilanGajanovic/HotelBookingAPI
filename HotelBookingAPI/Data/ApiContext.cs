b92e5119eb42acacee7415f5d72c1dc81363a0fe
Milan_Gajanovic <milan.gajanovic@nites.eu>
2024-04-18 14:00:14 +0200

    Add project files.

---
+++ HotelBookingAPI/Data/ApiContext.cs
@@ -0,0 +1,17 @@
+﻿using HotelBookingAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace HotelBookingAPI.Data
+{
+    public class ApiContext: DbContext
+    {
+
+        public DbSet<HotelBooking> Bookings { get; set; }   
+        public ApiContext(DbContextOptions<ApiContext> options)
+        :base(options)
+        { 
+        }
+       
+    }
+}
